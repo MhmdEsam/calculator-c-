4282ba897d4428c0f364668785e0902a106f6397
MhmdEsam <64297258+MhmdEsam@users.noreply.github.com>
2022-06-05 12:28:48 +0200

    Add files via upload

---
+++ Calculator/Calculator2/Calculator2/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace Calculator2
+{
+    partial class DesignCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DesignCalculator));
+            this.CalculatorDesign = new System.Windows.Forms.Label();
+            this.Design1 = new System.Windows.Forms.Button();
+            this.Design2 = new System.Windows.Forms.Button();
+            this.Design3 = new System.Windows.Forms.Button();
+            this.Design4 = new System.Windows.Forms.Button();
+            this.Design5 = new System.Windows.Forms.Button();
+            this.Design6 = new System.Windows.Forms.Button();
+            this.Design7 = new System.Windows.Forms.Button();
+            this.Design8 = new System.Windows.Forms.Button();
+            this.Design9 = new System.Windows.Forms.Button();
+            this.Design0 = new System.Windows.Forms.Button();
+            this.DesignDot = new System.Windows.Forms.Button();
+            this.DesignEqual = new System.Windows.Forms.Button();
+            this.DesignPlus = new System.Windows.Forms.Button();
+            this.DesignMinus = new System.Windows.Forms.Button();
+            this.DesignMultiply = new System.Windows.Forms.Button();
+            this.DesignDivide = new System.Windows.Forms.Button();
+            this.DesignDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CalculatorDesign
+            // 
+            this.CalculatorDesign.BackColor = System.Drawing.Color.White;
+            this.CalculatorDesign.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalculatorDesign.ForeColor = System.Drawing.Color.Black;
+            this.CalculatorDesign.Location = new System.Drawing.Point(8, 9);
+            this.CalculatorDesign.Name = "CalculatorDesign";
+            this.CalculatorDesign.Size = new System.Drawing.Size(324, 80);
+            this.CalculatorDesign.TabIndex = 0;
+            this.CalculatorDesign.Text = "0";
+            this.CalculatorDesign.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Design1
+            // 
+            this.Design1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design1.Location = new System.Drawing.Point(11, 115);
+            this.Design1.Name = "Design1";
+            this.Design1.Size = new System.Drawing.Size(75, 41);
+            this.Design1.TabIndex = 1;
+            this.Design1.Text = "1";
+            this.Design1.UseVisualStyleBackColor = true;
+            this.Design1.Click += new System.EventHandler(this.Design1_Click);
+            // 
+            // Design2
+            // 
+            this.Design2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design2.Location = new System.Drawing.Point(92, 115);
+            this.Design2.Name = "Design2";
+            this.Design2.Size = new System.Drawing.Size(75, 41);
+            this.Design2.TabIndex = 2;
+            this.Design2.Text = "2";
+            this.Design2.UseVisualStyleBackColor = true;
+            this.Design2.Click += new System.EventHandler(this.Design2_Click);
+            // 
+            // Design3
+            // 
+            this.Design3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design3.Location = new System.Drawing.Point(173, 115);
+            this.Design3.Name = "Design3";
+            this.Design3.Size = new System.Drawing.Size(75, 41);
+            this.Design3.TabIndex = 3;
+            this.Design3.Text = "3";
+            this.Design3.UseVisualStyleBackColor = true;
+            this.Design3.Click += new System.EventHandler(this.Design3_Click);
+            // 
+            // Design4
+            // 
+            this.Design4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design4.Location = new System.Drawing.Point(11, 162);
+            this.Design4.Name = "Design4";
+            this.Design4.Size = new System.Drawing.Size(75, 41);
+            this.Design4.TabIndex = 4;
+            this.Design4.Text = "4";
+            this.Design4.UseVisualStyleBackColor = true;
+            this.Design4.Click += new System.EventHandler(this.Design4_Click);
+            // 
+            // Design5
+            // 
+            this.Design5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design5.Location = new System.Drawing.Point(92, 162);
+            this.Design5.Name = "Design5";
+            this.Design5.Size = new System.Drawing.Size(75, 41);
+            this.Design5.TabIndex = 5;
+            this.Design5.Text = "5";
+            this.Design5.UseVisualStyleBackColor = true;
+            this.Design5.Click += new System.EventHandler(this.Design5_Click);
+            // 
+            // Design6
+            // 
+            this.Design6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design6.Location = new System.Drawing.Point(173, 162);
+            this.Design6.Name = "Design6";
+            this.Design6.Size = new System.Drawing.Size(75, 41);
+            this.Design6.TabIndex = 6;
+            this.Design6.Text = "6";
+            this.Design6.UseVisualStyleBackColor = true;
+            this.Design6.Click += new System.EventHandler(this.Design6_Click);
+            // 
+            // Design7
+            // 
+            this.Design7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design7.Location = new System.Drawing.Point(11, 209);
+            this.Design7.Name = "Design7";
+            this.Design7.Size = new System.Drawing.Size(75, 41);
+            this.Design7.TabIndex = 7;
+            this.Design7.Text = "7";
+            this.Design7.UseVisualStyleBackColor = true;
+            this.Design7.Click += new System.EventHandler(this.Design7_Click);
+            // 
+            // Design8
+            // 
+            this.Design8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design8.Location = new System.Drawing.Point(92, 209);
+            this.Design8.Name = "Design8";
+            this.Design8.Size = new System.Drawing.Size(75, 41);
+            this.Design8.TabIndex = 8;
+            this.Design8.Text = "8";
+            this.Design8.UseVisualStyleBackColor = true;
+            this.Design8.Click += new System.EventHandler(this.Design8_Click);
+            // 
+            // Design9
+            // 
+            this.Design9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design9.Location = new System.Drawing.Point(173, 209);
+            this.Design9.Name = "Design9";
+            this.Design9.Size = new System.Drawing.Size(75, 41);
+            this.Design9.TabIndex = 9;
+            this.Design9.Text = "9";
+            this.Design9.UseVisualStyleBackColor = true;
+            this.Design9.Click += new System.EventHandler(this.Design9_Click);
+            // 
+            // Design0
+            // 
+            this.Design0.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Design0.Location = new System.Drawing.Point(11, 256);
+            this.Design0.Name = "Design0";
+            this.Design0.Size = new System.Drawing.Size(162, 41);
+            this.Design0.TabIndex = 10;
+            this.Design0.Text = "0";
+            this.Design0.UseVisualStyleBackColor = true;
+            this.Design0.Click += new System.EventHandler(this.Design0_Click);
+            // 
+            // DesignDot
+            // 
+            this.DesignDot.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesignDot.Location = new System.Drawing.Point(179, 256);
+            this.DesignDot.Name = "DesignDot";
+            this.DesignDot.Size = new System.Drawing.Size(75, 41);
+            this.DesignDot.TabIndex = 11;
+            this.DesignDot.Text = ".";
+            this.DesignDot.UseVisualStyleBackColor = true;
+            this.DesignDot.Click += new System.EventHandler(this.DesignDot_Click);
+            // 
+            // DesignEqual
+            // 
+            this.DesignEqual.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesignEqual.Location = new System.Drawing.Point(179, 303);
+            this.DesignEqual.Name = "DesignEqual";
+            this.DesignEqual.Size = new System.Drawing.Size(156, 41);
+            this.DesignEqual.TabIndex = 12;
+            this.DesignEqual.Text = "=";
+            this.DesignEqual.UseVisualStyleBackColor = true;
+            this.DesignEqual.Click += new System.EventHandler(this.DesignEqual_Click);
+            // 
+            // DesignPlus
+            // 
+            this.DesignPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesignPlus.Location = new System.Drawing.Point(260, 256);
+            this.DesignPlus.Name = "DesignPlus";
+            this.DesignPlus.Size = new System.Drawing.Size(75, 41);
+            this.DesignPlus.TabIndex = 13;
+            this.DesignPlus.Text = "+";
+            this.DesignPlus.UseVisualStyleBackColor = true;
+            this.DesignPlus.Click += new System.EventHandler(this.DesignPlus_Click);
+            // 
+            // DesignMinus
+            // 
+            this.DesignMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesignMinus.Location = new System.Drawing.Point(260, 209);
+            this.DesignMinus.Name = "DesignMinus";
+            this.DesignMinus.Size = new System.Drawing.Size(75, 41);
+            this.DesignMinus.TabIndex = 14;
+            this.DesignMinus.Text = "-";
+            this.DesignMinus.UseVisualStyleBackColor = true;
+            this.DesignMinus.Click += new System.EventHandler(this.DesignMinus_Click);
+            // 
+            // DesignMultiply
+            // 
+            this.DesignMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesignMultiply.Location = new System.Drawing.Point(260, 162);
+            this.DesignMultiply.Name = "DesignMultiply";
+            this.DesignMultiply.Size = new System.Drawing.Size(75, 41);
+            this.DesignMultiply.TabIndex = 15;
+            this.DesignMultiply.Text = "*";
+            this.DesignMultiply.UseVisualStyleBackColor = true;
+            this.DesignMultiply.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // DesignDivide
+            // 
+            this.DesignDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesignDivide.Location = new System.Drawing.Point(260, 115);
+            this.DesignDivide.Name = "DesignDivide";
+            this.DesignDivide.Size = new System.Drawing.Size(75, 41);
+            this.DesignDivide.TabIndex = 16;
+            this.DesignDivide.Text = "/";
+            this.DesignDivide.UseVisualStyleBackColor = true;
+            this.DesignDivide.Click += new System.EventHandler(this.DesignDivide_Click);
+            // 
+            // DesignDelete
+            // 
+            this.DesignDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesignDelete.Location = new System.Drawing.Point(11, 303);
+            this.DesignDelete.Name = "DesignDelete";
+            this.DesignDelete.Size = new System.Drawing.Size(162, 41);
+            this.DesignDelete.TabIndex = 17;
+            this.DesignDelete.Text = "Delete";
+            this.DesignDelete.UseVisualStyleBackColor = true;
+            this.DesignDelete.Click += new System.EventHandler(this.DesignDelete_Click);
+            // 
+            // DesignCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(344, 348);
+            this.Controls.Add(this.DesignDelete);
+            this.Controls.Add(this.DesignDivide);
+            this.Controls.Add(this.DesignMultiply);
+            this.Controls.Add(this.DesignMinus);
+            this.Controls.Add(this.DesignPlus);
+            this.Controls.Add(this.DesignEqual);
+            this.Controls.Add(this.DesignDot);
+            this.Controls.Add(this.Design0);
+            this.Controls.Add(this.Design9);
+            this.Controls.Add(this.Design8);
+            this.Controls.Add(this.Design7);
+            this.Controls.Add(this.Design6);
+            this.Controls.Add(this.Design5);
+            this.Controls.Add(this.Design4);
+            this.Controls.Add(this.Design3);
+            this.Controls.Add(this.Design2);
+            this.Controls.Add(this.Design1);
+            this.Controls.Add(this.CalculatorDesign);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DesignCalculator";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CalculatorDesign;
+        private System.Windows.Forms.Button Design1;
+        private System.Windows.Forms.Button Design2;
+        private System.Windows.Forms.Button Design3;
+        private System.Windows.Forms.Button Design4;
+        private System.Windows.Forms.Button Design5;
+        private System.Windows.Forms.Button Design6;
+        private System.Windows.Forms.Button Design7;
+        private System.Windows.Forms.Button Design8;
+        private System.Windows.Forms.Button Design9;
+        private System.Windows.Forms.Button Design0;
+        private System.Windows.Forms.Button DesignDot;
+        private System.Windows.Forms.Button DesignEqual;
+        private System.Windows.Forms.Button DesignPlus;
+        private System.Windows.Forms.Button DesignMinus;
+        private System.Windows.Forms.Button DesignMultiply;
+        private System.Windows.Forms.Button DesignDivide;
+        private System.Windows.Forms.Button DesignDelete;
+    }
+}
+
---
+++ Calculator/Calculator2/Calculator2/Form1.cs
@@ -0,0 +1,184 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator2
+{
+    public partial class DesignCalculator : Form
+    {
+        string first = "";
+        string second = "";
+        string userinput = "";
+        char function;
+        double result = 0.0;
+        
+        public DesignCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            function = '*';
+            first = userinput;
+            userinput = "";
+        }
+
+        private void DesignDivide_Click(object sender, EventArgs e)
+        {
+            function = '/';
+            first = userinput;
+            userinput = "";
+        }
+
+        private void Design1_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "1";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Design2_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "2";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void DesignDot_Click(object sender, EventArgs e)
+        {
+            
+            userinput += ".";
+            
+        }
+
+        private void Design3_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "3";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void Design4_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "4";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void Design5_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "5";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void Design6_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "6";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void Design7_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "7";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void Design8_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "8";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void Design9_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "9";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void DesignMinus_Click(object sender, EventArgs e)
+        {
+            function = '-';
+            first = userinput;
+            userinput = "";
+        }
+
+        private void Design0_Click(object sender, EventArgs e)
+        {
+            CalculatorDesign.Text = "";
+            userinput += "0";
+            CalculatorDesign.Text += userinput;
+        }
+
+        private void DesignPlus_Click(object sender, EventArgs e)
+        {
+            function = '+';
+            first = userinput;
+            userinput = "";
+        }
+
+        private void DesignEqual_Click(object sender, EventArgs e)
+        {
+            second = userinput;
+            double firstnum, secondnum;
+            firstnum = Convert.ToDouble(first);
+            secondnum = Convert.ToDouble(second);
+            switch (function)
+            {
+                //plus
+                case '+':
+                    result = firstnum + secondnum;
+                    CalculatorDesign.Text = result.ToString();
+                    break;
+                //minus
+                case '-':
+                     result = firstnum - secondnum;
+                    CalculatorDesign.Text = result.ToString();
+                    break;
+                //muliyply
+                case '*':
+                    result = firstnum * secondnum;
+                    CalculatorDesign.Text = result.ToString();
+                    break;
+                //divide
+                case '/':
+                    result = firstnum / secondnum;
+                    CalculatorDesign.Text = result.ToString();
+                    break;
+            }
+
+        }
+
+        private void DesignDelete_Click(object sender, EventArgs e)
+        {
+            first = "";
+            second = "";
+            userinput = "";
+            result = 0.0;
+            CalculatorDesign.Text = "Calculator";
+
+        }
+    }
+}
