a36f799201587e1b318275a26edc61bf8d24e11f
MhmdEsam <64297258+MhmdEsam@users.noreply.github.com>
2022-06-05 12:24:43 +0200

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char cal;
+            double num1, num2;
+        start:
+            Console.WriteLine("Choose what you want ( + , - , / , * )");
+            cal = Console.ReadLine()[0];
+
+
+            Console.WriteLine("Enter a numbers");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            switch (cal)
+            {
+                case '+':
+                    Console.WriteLine("{0} + {1} = {2}", num1, num2, (num1 + num2));
+                    break;
+
+                case '-':
+                    Console.WriteLine("{0} - {1} = {2}", num1, num2, (num1 - num2));
+                    break;
+
+                case '/':
+                    if (num2 == 0.0)
+                        Console.WriteLine("Error");
+                    else
+                        Console.WriteLine("{0} / {1} = {2}", num1, num2, (num1 / num2));
+                    break;
+                case '*':
+                    Console.WriteLine("{0} * {1} = {2}", num1, num2, (num1 * num2));
+                    break;
+
+                default:
+                    Console.WriteLine("{0}", cal);
+                    break;
+            }
+            Console.WriteLine("Do you want more calculations? Yes or No If yes, press y If not, press any key ");
+
+            string rst = Console.ReadLine();
+
+            if (rst == "y")
+            {
+                goto start;
+            }
+            else
+            {
+
+            }
+            Console.WriteLine("Thanks press any key to close");
+            Console.ReadKey(true);
+        }
+    }
+}
